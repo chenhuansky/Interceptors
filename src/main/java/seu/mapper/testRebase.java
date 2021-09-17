2ddf56ebc93603a038418fa292cacb99016a4976
chenhuan <fengchenhuan@meituan.com>
2021-09-17 15:56:38 +0800

    fix:1->fix:2

--- src/main/java/seu/mapper/testRebase.java
+++ src/main/java/seu/mapper/testRebase.java
@@ -2,6 +2,7 @@ package seu.mapper;
 
 public class testRebase {
     public static void main(String[] args) {
-        System.out.println("Rebase");
+
+        System.out.println("Rebase again");
     }
 }
